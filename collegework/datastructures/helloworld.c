d09c15632c588188e4cfa0264f9b81caf190d930
Atharva Auti <bm.aatharvauti@protonmail.ch>
2021-10-05 15:29:02 +0530

    September Labs

---
+++ collegework/datastructures/helloworld.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void main () {
+    printf("\n Hello World! \n C Program Test! \n Ran on Visual Studio Code on WSL Server, Ubuntu \n Windows 11 Developer Build \n ~ aatharvauti \n\n");
+
+}
