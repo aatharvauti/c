207fb8218e94c844e5b374c954f5b97124087332
aatharvauti <bm.aatharvauti@protonmail.ch>
2021-11-03 10:20:13 +0530

    Flood Fill Algo Fix (Again, Again, Again, Again)

--- collegework/computergraphics/flood-fill-rectangle.c
+++ collegework/computergraphics/flood-fill-rectangle.c
@@ -16,11 +16,13 @@ int main()
     detectgraph(&gd,&gm);
     initgraph(&gd, &gm, (char*)"");
     cleardevice();
+
     setcolor(WHITE);
     rectangle(x1,y1,x2,y2);
     flood(x1+1,y1+1,2,0);
 
     delay(60000);
+    closegraph();
     return 0;
 }
 
@@ -28,7 +30,6 @@ void flood(int x, int y, int new, int old)
 {
     if (getpixel(x, y) == old)
     {
-        delay(10);
         putpixel(x,y,new);
         //bottom, top, right, left
         flood(x + 1, y, new, old);
