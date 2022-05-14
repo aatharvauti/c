5b5c661e90d4b470113f9f33ee2368f02eb83736
aatharvauti <bm.aatharvauti@protonmail.ch>
2022-05-14 20:22:33 +0100

    Rename

--- collegework/datastructures/helloworld.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-void main () {
-    printf("\n Hello World! \n C Program Test! \n Ran on Visual Studio Code on WSL Server, Ubuntu \n Windows 11 Developer Build \n ~ aatharvauti \n\n");
-
-}
--- collegework/fundamentals/1. operators/helloworld.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-	printf("HELLO WORLD!\n");
-	printf("This is my first C Program\n");
-	return 0;
-}
